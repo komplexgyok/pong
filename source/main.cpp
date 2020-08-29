572d61f36396cda39e971998e58d45c96b30711a
komplexgyok <komplexgyok@gmail.com>
2020-08-29 11:14:22 +0200

    Added GLFW.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Pong" << std::endl;
+	// Initialize GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
