a2522bf4480285187df38bc967181e23cfef1d78
komplexgyok <komplexgyok@gmail.com>
2020-09-09 19:40:26 +0200

    Fixed the windows build concerning the glm library.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 mkdir build 2> NUL
 g++ source/*.cpp libraries/glad/src/glad.c^
  -I"libraries/glad/include"^
- -I"libraries/glm"^
  -I"libraries-win/glfw/include" -L"libraries-win/glfw/lib"^
+ -I"libraries-win/glm/include"^
  -lglfw3 -lgdi32^
  -mwindows^
  -o build/pong.exe
