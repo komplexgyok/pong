beda51aa3370be2d6cd78cb4b3c7080e992accb0
komplexgyok <komplexgyok@gmail.com>
2020-09-09 19:33:06 +0200

    Added glm to the windows build batch file.

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 mkdir build 2> NUL
 g++ source/*.cpp libraries/glad/src/glad.c^
  -I"libraries/glad/include"^
+ -I"libraries/glm"^
  -I"libraries-win/glfw/include" -L"libraries-win/glfw/lib"^
  -lglfw3 -lgdi32^
  -mwindows^
