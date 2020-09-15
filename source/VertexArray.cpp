440f037d7fa46d87beb9550496857bb183c46f87
komplexgyok <komplexgyok@gmail.com>
2020-09-15 10:38:40 +0200

    Separated the VertexBuffer and IndexBuffer from the VertexArray.

--- source/VertexArray.cpp
+++ source/VertexArray.cpp
@@ -5,8 +5,6 @@
  **********************************************************************************************************************/
 VertexArray::VertexArray()
 	: id_ {0}
-	, vertexBuffer_ {new VertexBuffer()}
-	, indexBuffer_ {new IndexBuffer()}
 {
 	glGenVertexArrays(1, &id_);
 }
@@ -17,29 +15,6 @@ VertexArray::VertexArray()
 VertexArray::~VertexArray()
 {
 	glDeleteVertexArrays(1, &id_);
-}
-
-/***********************************************************************************************************************
- * Binds the vertex and index buffers to the vertex array.
- * Passes the vertex and index data to them.
- * Configures the vertex attributes.
- *
- * @param float *vertexData         Vertex data.
- * @param unsigned int vertexSize   Size of the vertex data in bytes.
- * @param unsigned int *indexData   Index data.
- * @param unsigned int indexSize    Size of the index data in bytes.
- *
- * @return void
- **********************************************************************************************************************/
-void VertexArray::configure(float *vertexData, unsigned int vertexSize, unsigned int *indexData, unsigned int indexSize) const
-{
-	bind();
-	vertexBuffer_->copy(vertexData, vertexSize);
-	indexBuffer_->copy(indexData, indexSize);
-
-	// Position
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 }
 
 /***********************************************************************************************************************
--- source/VertexArray.h
+++ source/VertexArray.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <glad/glad.h>
-#include "IndexBuffer.h"
-#include "VertexBuffer.h"
 
 /***********************************************************************************************************************
  * VertexArray class.
@@ -13,15 +11,13 @@
 class VertexArray
 {
 public:
+	// Constructor and destructor
 	VertexArray();
 	~VertexArray();
 
-	void configure(float *vertexData, unsigned int vertexSize, unsigned int *indexData, unsigned int indexSize) const;
 	void bind() const;
 	void unbind() const;
 
 private:
-	unsigned int id_;              // OpenGL vertex array object ID
-	VertexBuffer *vertexBuffer_;   // Vertex buffer object
-	IndexBuffer *indexBuffer_;     // Index buffer object
+	unsigned int id_;   // OpenGL vertex array object ID
 };
