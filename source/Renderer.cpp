95004bfe11681bcf4817e468bbb96589ba4d9637
komplexgyok <komplexgyok@gmail.com>
2020-09-15 11:01:44 +0200

    Renderer rewrite. It now batch renders instead of single sprite rendering.

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -1,37 +1,56 @@
 #include "Renderer.h"
 
 /***********************************************************************************************************************
- * Constructor.
- *
- * @param int width    Width of the window in pixels.
- * @param int height   Height of the window in pixels.
+ * Constructor. Sets up the renderer for 2D quad rendering.
  **********************************************************************************************************************/
-Renderer::Renderer(int width, int height)
-	: shader_ {new Shader("../resources/shaders/sprite-vs.glsl", "../resources/shaders/sprite-fs.glsl")}
-	, vertexArray_ {new VertexArray()}
+Renderer::Renderer()
+	: buffer_ {nullptr}
+	, vertexArray_ {nullptr}
+	, vertexBuffer_ {nullptr}
+	, indexBuffer_ {nullptr}
+	, indexCount_ {0}
 {
-	// Vertex data
-	float vertices[] = {
-		// Positions
-		1.0f, 1.0f,   // Top right
-		1.0f, 0.0f,   // Bottom right
-		0.0f, 0.0f,   // Bottom left
-		0.0f, 1.0f    // Top left
-	};
+	// Create and bind the vertex array
+	vertexArray_ = new VertexArray();
+	vertexArray_->bind();
 
-	// Index data
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3
-	};
+	// Create and bind the vertex buffer
+	// We don't have the data yet, so we only allocate the memory
+	vertexBuffer_ = new VertexBuffer();
+	vertexBuffer_->data(nullptr, RENDERER_BUFFER_SIZE);
 
-	// Copy the data into the correct buffers and set the vertex attributes
-	vertexArray_->configure(vertices, sizeof(vertices), indices, sizeof(indices));
+	// Configure vertex attributes
+	// Position
+	glEnableVertexAttribArray(SHADER_POSITION_INDEX);
+	glVertexAttribPointer(SHADER_POSITION_INDEX, 2, GL_FLOAT, GL_FALSE, RENDERER_QUAD_VERTEX_SIZE, nullptr);
 
-	// Set an orthographic projection
-	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);
-	shader_->use();
-	shader_->setMatrix4("uProjection", projection);
+	// Color
+	glEnableVertexAttribArray(SHADER_COLOR_INDEX);
+	glVertexAttribPointer(SHADER_COLOR_INDEX, 4, GL_FLOAT, GL_FALSE, RENDERER_QUAD_VERTEX_SIZE, (const GLvoid*)(offsetof(QuadVertex, color)));
+
+	unsigned int indices[RENDERER_INDICES_SIZE];
+	unsigned int offset = 0;
+
+	// Set the index data
+	for (int i = 0; i < RENDERER_INDICES_SIZE; i += 6) {
+		indices[i]   = offset + 0;
+		indices[i+1] = offset + 1;
+		indices[i+2] = offset + 2;
+		indices[i+3] = offset + 2;
+		indices[i+4] = offset + 3;
+		indices[i+5] = offset + 0;
+
+		offset += 4;
+	}
+
+	// Copy the index data to the buffer's memory
+	indexBuffer_ = new IndexBuffer();
+	indexBuffer_->data(indices, RENDERER_INDICES_SIZE);
+
+	// Unbind everything
+	vertexArray_->unbind();
+	vertexBuffer_->unbind();
+	indexBuffer_->unbind();
 }
 
 /***********************************************************************************************************************
@@ -39,34 +58,82 @@ Renderer::Renderer(int width, int height)
  **********************************************************************************************************************/
 Renderer::~Renderer()
 {
+	delete buffer_;
 	delete vertexArray_;
-	delete shader_;
+	delete vertexBuffer_;
+	delete indexBuffer_;
 }
 
 /***********************************************************************************************************************
- * Draws a 2D colored rectangle.
- *
- * @param const glm::vec2 &position   Position of the sprite, (x,y) coordinate.
- * @param const glm::vec2 &size       Size of the sprite in pixels (width, height).
- * @param const glm::vec4 &color      Color of the sprite (RGBA). White is the default color if it isn't specified.
+ * Sets a pointer to the memory content of the buffer. Call it before adding renderables.
  *
  * @return void
  **********************************************************************************************************************/
-void Renderer::drawSprite(const glm::vec2 &position, const glm::vec2 &size, const glm::vec4 &color)
+void Renderer::begin()
 {
-	shader_->use();
+	vertexBuffer_->bind();
+	buffer_ = (QuadVertex*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+}
 
-	// Define the model matrix
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(position, 0.0f));
-	model = glm::scale(model, glm::vec3(size, 1.0f));
+/***********************************************************************************************************************
+ * Unmaps the pointer mapped by begin(). Call it after adding the renderables.
+ *
+ * @return void
+ **********************************************************************************************************************/
+void Renderer::end()
+{
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+	vertexBuffer_->unbind();
+}
 
-	// Set the uniforms
-	shader_->setMatrix4("uModel", model);
-	shader_->setVector4("uColor", color);
+/***********************************************************************************************************************
+ * Adds a new renderable object to the renderer's buffer. Anything added will be drawn with the next render() call.
+ *
+ * @param const Renderable *renderable   Renderable object.
+ *
+ * @return void
+ **********************************************************************************************************************/
+void Renderer::add(const Renderable *renderable)
+{
+	const glm::vec2 position = renderable->getPosition();
+	const glm::vec2 size     = renderable->getSize();
+	const glm::vec4 color    = renderable->getColor();
 
-	// Draw the rectangle
+	// Fill the buffer with the quad vertex data
+	buffer_->position = position;
+	buffer_->color = color;
+	buffer_++;
+
+	buffer_->position = glm::vec2(position.x, position.y + size.y);
+	buffer_->color = color;
+	buffer_++;
+
+	buffer_->position = glm::vec2(position.x + size.x, position.y + size.y);
+	buffer_->color = color;
+	buffer_++;
+
+	buffer_->position = glm::vec2(position.x + size.x, position.y);
+	buffer_->color = color;
+	buffer_++;
+
+	// We use 6 indices to draw a quad
+	indexCount_ += 6;
+}
+
+/***********************************************************************************************************************
+ * Draws all the objects stored inside the renderer's buffer.
+ *
+ * @return void
+ **********************************************************************************************************************/
+void Renderer::render()
+{
 	vertexArray_->bind();
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	indexBuffer_->bind();
+
+	glDrawElements(GL_TRIANGLES, indexCount_, GL_UNSIGNED_INT, nullptr);
+
 	vertexArray_->unbind();
+	indexBuffer_->unbind();
+
+	indexCount_ = 0;
 }
--- source/Renderer.h
+++ source/Renderer.h
@@ -3,8 +3,30 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "Shader.h"
+#include "IndexBuffer.h"
+#include "Renderable.h"
 #include "VertexArray.h"
+#include "VertexBuffer.h"
+
+/***********************************************************************************************************************
+ * Data type storing the data of a single quad vertex.
+ **********************************************************************************************************************/
+struct QuadVertex
+{
+	glm::vec2 position;
+	glm::vec4 color;
+};
+
+// Renderer constants
+const unsigned int RENDERER_MAX_SPRITES      = 10;
+const unsigned int RENDERER_QUAD_VERTEX_SIZE = sizeof(QuadVertex);
+const unsigned int RENDERER_SPRITE_SIZE      = RENDERER_QUAD_VERTEX_SIZE * 4;
+const unsigned int RENDERER_BUFFER_SIZE      = RENDERER_SPRITE_SIZE * RENDERER_MAX_SPRITES;
+const unsigned int RENDERER_INDICES_SIZE     = RENDERER_MAX_SPRITES * 6;
+
+// Vertex attribute constants
+const unsigned int SHADER_POSITION_INDEX = 0;
+const unsigned int SHADER_COLOR_INDEX    = 1;
 
 /***********************************************************************************************************************
  * Renderer class.
@@ -14,12 +36,19 @@
 class Renderer
 {
 public:
-	Renderer(int width, int height);
+	// Constructor and destructor
+	Renderer();
 	~Renderer();
 
-	void drawSprite(const glm::vec2 &position, const glm::vec2 &size, const glm::vec4 &color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+	void begin();
+	void end();
+	void add(const Renderable *renderable);
+	void render();
 
 private:
-	Shader *shader_;             // Shader object
-	VertexArray *vertexArray_;   // Vertex array object
+	QuadVertex *buffer_;           // Buffer for storing all the sprite data on a layer
+	VertexArray *vertexArray_;     // Vertex array object
+	VertexBuffer *vertexBuffer_;   // Vertex buffer object
+	IndexBuffer *indexBuffer_;     // Index buffer object
+	unsigned int indexCount_;      // Number of total indices
 };
