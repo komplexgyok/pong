3bc8f674abf2364bc722fc76b8878a9c4e285d85
komplexgyok <komplexgyok@gmail.com>
2020-09-15 18:15:42 +0200

    Shader changes to handle textured sprites too.

--- resources/shaders/sprite-fs.glsl
+++ resources/shaders/sprite-fs.glsl
@@ -1,10 +1,22 @@
 #version 330 core
 
+in vec2 sTextureCoordinate;
+in float sTextureId;
 in vec4 sColor;
 
 out vec4 color;
 
+uniform sampler2D uTextures[32];
+
 void main()
 {
-    color = sColor;
+    vec4 textureColor = sColor;
+
+    // Textured sprite
+    if (sTextureId > 0.0f) {
+        int textureId = int(sTextureId - 0.5);
+        textureColor = texture(uTextures[textureId], sTextureCoordinate);
+    }
+
+    color = textureColor;
 }
--- resources/shaders/sprite-vs.glsl
+++ resources/shaders/sprite-vs.glsl
@@ -1,14 +1,20 @@
 #version 330 core
 
 layout (location = 0) in vec2 position;
-layout (location = 1) in vec4 color;
+layout (location = 1) in vec2 textureCoordinate;
+layout (location = 2) in float textureId;
+layout (location = 3) in vec4 color;
 
+out vec2 sTextureCoordinate;
+out float sTextureId;
 out vec4 sColor;
 
 uniform mat4 uProjection;
 
 void main()
 {
+    sTextureCoordinate = textureCoordinate;
+    sTextureId = textureId;
     sColor = color;
     gl_Position = uProjection * vec4(position, 0.0f, 1.0f);
 }
