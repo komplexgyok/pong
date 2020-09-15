a38afb6700872b81e50906c446361a56c5054db9
komplexgyok <komplexgyok@gmail.com>
2020-09-15 11:25:52 +0200

    Shader for colored 2D rectangles.

--- resources/shaders/sprite-vs.glsl
+++ resources/shaders/sprite-vs.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec2 position;
+layout (location = 1) in vec4 color;
 
-uniform mat4 uModel;
+out vec4 sColor;
+
 uniform mat4 uProjection;
 
 void main()
 {
-    gl_Position = uProjection * uModel * vec4(position, 0.0f, 1.0f);
+    sColor = color;
+    gl_Position = uProjection * vec4(position, 0.0f, 1.0f);
 }
