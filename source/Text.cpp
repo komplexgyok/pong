40c4630e090df02625c8479b827981dfa0f940c5
komplexgyok <komplexgyok@gmail.com>
2020-10-11 14:12:27 +0200

    Added text animations.

--- source/Text.cpp
+++ source/Text.cpp
@@ -10,10 +10,13 @@
  * @param Font *font                Font object.
  * @param const glm::vec4 &color    Color of the text. White is the default.
  **********************************************************************************************************************/
-Text::Text(const std::string &text, float x, float y, Font *font, const glm::vec4 &color)
+Text::Text(const std::string &text, float x, float y, Font *font, float scale, const glm::vec4 &color)
 	: Renderable(glm::vec2(x, y), glm::vec2(0.0f), color)
 	, text_ {text}
 	, font_ {font}
+	, scale_ {scale}
+	, alpha_ {0.0f}
+	, fade_ {1.0f}
 {}
 
 /***********************************************************************************************************************
@@ -26,4 +29,72 @@ Text::Text(const std::string &text, float x, float y, Font *font, const glm::vec
 void Text::submit(Renderer *renderer) const
 {
 	renderer->addText(this);
+}
+
+/***********************************************************************************************************************
+ * Animation. The text fades in then it fades out.
+ *
+ * @param float deltaTime   Timestep.
+ *
+ * @return bool   Is the animation done?
+ **********************************************************************************************************************/
+bool Text::fadeInOut(float deltaTime)
+{
+	bool isDone = false;
+
+	alpha_ += 0.3f * fade_ * deltaTime;
+	setColor(glm::vec4(color.r, color.g, color.b, alpha_));
+
+	if (alpha_ >= 1.0f) {
+		fade_ *= -1;
+	}
+
+	if (alpha_ <= 0.0f) {
+		isDone = true;
+	}
+
+	return isDone;
+}
+
+/***********************************************************************************************************************
+ * Animation. The text fades in.
+ *
+ * @param float deltaTime   Timestep.
+ *
+ * @return bool   Is the animation done?
+ **********************************************************************************************************************/
+bool Text::fadeIn(float deltaTime)
+{
+	bool isDone = false;
+
+	alpha_ += 0.3f * fade_ * deltaTime;
+	setColor(glm::vec4(color.r, color.g, color.b, alpha_));
+
+	if (alpha_ >= 1.0f) {
+		alpha_ = 1.0f;
+		isDone = true;
+	}
+
+	return isDone;
+}
+
+/***********************************************************************************************************************
+ * Animation. The text moves up.
+ *
+ * @param float deltaTime   Timestep.
+ *
+ * @return bool   Is the animation done?
+ **********************************************************************************************************************/
+bool Text::moveUp(float deltaTime)
+{
+	bool isDone = false;
+
+	if (position.y > 50.0f) {
+		setPositionY(position.y - 200.0f * deltaTime);
+	}
+	else {
+		isDone = true;
+	}
+
+	return isDone;
 }
--- source/Text.h
+++ source/Text.h
@@ -10,15 +10,27 @@ class Text : public Renderable
 {
 public:
 	// Constructor
-	Text(const std::string &text, float x, float y, Font *font, const glm::vec4 &color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+	Text(const std::string &text, float x, float y, Font *font, float scale, const glm::vec4 &color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 
 	void submit(Renderer *renderer) const override;
+
+	// Animations
+	bool fadeInOut(float deltaTime);
+	bool fadeIn(float deltaTime);
+	bool moveUp(float deltaTime);
 
 	// Getters
 	[[nodiscard]] inline const std::string & getText() const { return text_; }
 	[[nodiscard]] inline Font * getFont()              const { return font_; }
+	[[nodiscard]] inline float getScale()              const { return scale_; }
+
+	// Setter
+	void setAlpha(float alpha) { alpha_ = alpha; }
 
 private:
-	std::string text_;
-	Font *font_;
+	std::string text_;   // Text
+	Font *font_;         // Font object
+	float scale_;        // Scale of the text
+	float alpha_;        // Alpha value
+	float fade_;         // Fade helper
 };
