5299cd9ca82d47acfac89c88e19d344fa098fca2
komplexgyok <komplexgyok@gmail.com>
2020-10-11 14:30:32 +0200

    Updated windows build.

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 mkdir build 2> NUL
-g++ source/*.cpp libraries/glad/src/glad.c^
+g++ source/*.cpp libraries/glad/src/glad.c libraries/stb_image/src/stb_image.cpp^
  -I"libraries/glad/include"^
+ -I"libraries/stb_image/include"^
  -I"libraries-win/glfw/include" -L"libraries-win/glfw/lib"^
+ -I"libraries-win/freetype2/include" -L"libraries-win/freetype2/lib"^
  -I"libraries-win/glm/include"^
  -lglfw3 -lgdi32^
  -mwindows^
