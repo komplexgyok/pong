d94111f48ea3c3e0fb3efad008b5fab1c6cca7c7
komplexgyok <komplexgyok@gmail.com>
2020-10-11 14:50:55 +0200

    Windows build fix.

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 mkdir build 2> NUL
-g++ source/*.cpp libraries/glad/src/glad.c libraries/stb_image/src/stb_image.cpp^
+g++ source/*.cpp libraries/glad/src/glad.c^
  -I"libraries/glad/include"^
  -I"libraries/stb_image/include"^
  -I"libraries-win/glfw/include" -L"libraries-win/glfw/lib"^
  -I"libraries-win/freetype2/include" -L"libraries-win/freetype2/lib"^
  -I"libraries-win/glm/include"^
- -lglfw3 -lgdi32^
+ -lglfw3 -lgdi32 -lfreetype^
  -mwindows^
  -o build/pong.exe
+copy /Y "libraries-win\freetype2\lib\freetype.dll" "build\freetype.dll"
